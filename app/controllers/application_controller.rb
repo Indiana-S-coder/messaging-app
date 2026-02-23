625fe2ce811655e539dbd0da5104607afd9b8e35
bhartip2003 <bharti@volopay.co>
2026-02-23 21:15:49 +0530

    feat: implement authorization logic in ApplicationController and define routes for user registration and login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,15 @@ class ApplicationController < ActionController::API
       cursor: params[:cursor].is_a?(Hash) || params[:cursor].is_a?(ActionController::Parameters) ? params.require(:cursor).permit(:before_time, :before_id) : nil
     }
   end
+
+  def authorize_request
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+
+    decoded = JsonWebToken.decode(token)
+
+    @current_user = User.find(decoded[:user_id]) if decoded
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Unauthorized' }, status: :unauthorized
+  end
 end
