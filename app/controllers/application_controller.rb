753e887337ff60b5a1da9a85d4cbc6e98e0c109f
bhartip2003 <bharti@volopay.co>
2026-02-24 13:17:00 +0530

    refactor: add pagination_params method for improved pagination handling in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  private
+
+  def pagination_params
+    {
+      limit: params[:limit],
+      cursor: params[:cursor].is_a?(Hash) || params[:cursor].is_a?(ActionController::Parameters) ? params.require(:cursor).permit(:before_time, :before_id) : nil
+    }
+  end
 end
